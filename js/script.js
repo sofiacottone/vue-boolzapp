9376044b73f0c78cf5fec8d9196f6ba2ea6af171
sofiacottone <cottonesofia@outlook.it>
2024-04-03 20:20:19 +0200

    feat: add dynamic contact list with images and names

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Milestone 1
+// - Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Milestone 2
+// - Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// - Click sul contatto mostra la conversazione del contatto cliccato
+
 const { createApp } = Vue;
 
 createApp({
@@ -86,6 +93,11 @@ createApp({
     };
   },
   methods: {
+    getUserImage(index) {
+      const userImage = `img/avatar${this.contacts[index].avatar}.jpg`;
+      return userImage;
+      console.log(userImage);
+    }
   },
   mounted() {
   }
